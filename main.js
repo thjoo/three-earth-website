71d0000377c71199ff3fe249dc34ba3b9397d962
thjoo <thierry.joerin@protonmail.com>
2022-08-10 19:12:59 +0200

    img

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ const sphere = new THREE.Mesh(
     fragmentShader: fragmentShader,
     uniforms: {
       globeTexture: {
-        value: new THREE.TextureLoader().load("./img/globe.png"),
+        value: new THREE.TextureLoader().load("./public/globe.png"),
       },
     },
   })
